e6e81ecfbb2defb9a3394d1f4273b3c7bb5d99f1
jamescarloreto <jamescarl533@gmail.com>
2024-09-25 15:00:14 -0700

    Update Users.java

--- src/main/java/com/outdoor/connect/model/Users.java
+++ src/main/java/com/outdoor/connect/model/Users.java
@@ -55,5 +55,5 @@ public class Users implements Serializable {
 
     private String verificationCode;
 
-    private LocalDateTime verificationCodeExpiration;
+    private LocalDateTime verificationCodeExpirationDate;
 }
